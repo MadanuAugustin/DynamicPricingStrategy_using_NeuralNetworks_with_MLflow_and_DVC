0fc61bd1530705ffb2163dc5d5c64c2ca0206ca1
augustin <augustin7766@gmail.com>
2024-07-19 13:11:28 +0530

    dataValidation

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import sys
 from src import logger, CustomException
 from src.dynamicPricing.pipeline.stage_01_dataIngestion import DataIngestionTrainingPipeline
+from src.dynamicPricing.pipeline.stage_02_dataValidation import DataValidationTrainingPipeline
 
 
 
@@ -14,5 +15,17 @@ try:
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
     logger.info(f'----------------------stage {STAGE_NAME} completed ---------------------')
+except Exception as e:
+    raise CustomException(e, sys)
+
+
+
+STAGE_NAME = 'Data Validation Stage'
+
+try:
+    logger.info(f'-----------------stage {STAGE_NAME} started-----------------------')
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f'-----------------stage {STAGE_NAME} completed-----------------------')
 except Exception as e:
     raise CustomException(e, sys)
--- src/dynamicPricing/config/configuration.py
+++ src/dynamicPricing/config/configuration.py
@@ -6,7 +6,7 @@
 from src.dynamicPricing.utils.common import read_yaml, create_directories
 from src.dynamicPricing.constants import *
 from src import logger, CustomException
-from src.dynamicPricing.entity.config_entity import (DataIngestionConfig)
+from src.dynamicPricing.entity.config_entity import (DataIngestionConfig, DataValidationConfig)
 
 
 class ConfigurationManager:
@@ -37,3 +37,21 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+        
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir = config.root_dir,
+            local_data_file= config.local_data_file,
+            STATUS_FILE = config.STATUS_FILE,
+            all_schema = schema
+        )
+
+        return data_validation_config
